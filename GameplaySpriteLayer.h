962167ea9bb40c9d12781528c8519a7635ba1fb2
Rogelio E. Cardona-Rivera <me@rogel.io>
2013-01-26 12:22:14 -0500

    Skeleton for the SpriteLayer

---
+++ GameplaySpriteLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameplaySpriteLayer.h
+//  ggj13
+//
+//  Created by Rogelio E. Cardona-Rivera on 1/26/13.
+//  Copyright 2013 Sweet Carolina Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameplaySpriteLayer : CCLayer {
+    
+}
+
+@end

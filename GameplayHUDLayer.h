2902f24da1158854da1856523bde4d68992283a6
Rogelio E. Cardona-Rivera <me@rogel.io>
2013-01-26 21:49:14 -0500

    Added basic HUD Layer Skeleton and added a new Sprite, the Battery, which goes on the HUD.

---
+++ GameplayHUDLayer.h
@@ -0,0 +1,16 @@
+//
+//  GameplayHUDLayer.h
+//  ggj13
+//
+//  Created by Rogelio E. Cardona-Rivera on 1/26/13.
+//  Copyright 2013 Sweet Carolina Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameplayHUDLayer : CCLayer {
+    
+}
+
+@end
---
+++ GameplayHUDLayer.m
@@ -0,0 +1,14 @@
+//
+//  GameplayHUDLayer.m
+//  ggj13
+//
+//  Created by Rogelio E. Cardona-Rivera on 1/26/13.
+//  Copyright 2013 Sweet Carolina Games. All rights reserved.
+//
+
+#import "GameplayHUDLayer.h"
+
+
+@implementation GameplayHUDLayer
+
+@end
